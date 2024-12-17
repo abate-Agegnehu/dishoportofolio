ca7821336931942da44e13e273a4a0fe0f546575
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2024-12-17 17:42:24 +0300

    update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. At sed soluta repellat necessitatibus exercitationem repellendus reprehenderit, nesciunt non aspernatur odio quis accusantium officiis, ipsam quae velit, ipsum atque earum est!
+    </div>
+  );
+}
+
+export default App;
